fbe22037c8d7cfae0e02f7f785242606c6bf5de0
Your Name <e>
2023-09-06 13:28:45 +0300

    Podolsky 06-09-23

--- 153/1.js
+++ 153/1.js
@@ -1,7 +1,4 @@
-let dashString = '';
-
-for (let i = 0; i < 5; i++) {
-  dashString += '-';
+for (let i = 10; i <= 1000; i++) {
+  let firstDigit = Math.floor(i / 10);
+  console.log(firstDigit);
 }
-
-console.log(dashString);
--- 153/2.js
+++ 153/2.js
@@ -1,7 +1,6 @@
-let numberString = '';
-
-for (let i = 1; i <= 9; i++) {
-  numberString += i;
+for (let i = 10; i <= 1000; i++) {
+  let firstDigit = Math.floor(i / 10);
+  let secondDigit = i % 10;
+  let sum = firstDigit + secondDigit;
+  console.log(sum);
 }
-
-console.log(numberString);
